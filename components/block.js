ef5d164a6d82d63e57e84ee1937113b24a46b231
Ed <ed@polarity.io>
2021-06-09 11:42:13 -0400

    Reduce amount of data down in overlay window

--- components/block.js
+++ components/block.js
@@ -1,3 +1,19 @@
 polarity.export = PolarityComponent.extend({
-  details: Ember.computed.alias('block.data.details')
+  details: Ember.computed.alias('block.data.details'),
+  showAll: false,
+  initialResultSizeToShow: 5,
+  additionalResults: Ember.computed('block.data.details.results', 'showAll', 'initialResultSizeToShow', function(){
+
+  }),
+  pagedResults: Ember.computed('block.data.details.results', 'showAll', 'initialResultSizeToShow', function () {
+    if (this.get('showAll')) {
+      return this.get('block.data.details.results');
+    }
+    return this.get('block.data.details.results').slice(0, this.get('initialResultSizeToShow'));
+  }),
+  actions: {
+    toggleShowAll: function(){
+      this.toggleProperty('showAll');
+    }
+  }
 });

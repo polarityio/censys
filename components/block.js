dac47f9a2a67caf43ab7ef627c4485eb2a461406
Ed <ed@polarity.io>
2025-09-10 20:29:51 -0400

    Update for compatibility with v3 API

--- components/block.js
+++ components/block.js
@@ -1,11 +1,15 @@
 polarity.export = PolarityComponent.extend({
   details: Ember.computed.alias('block.data.details'),
+  resource: Ember.computed.alias('block.data.details.result.resource'),
+  timezone: Ember.computed('Intl', function () {
+    return Intl.DateTimeFormat().resolvedOptions().timeZone;
+  }),
   /**
    * details.result.operating_system can be an empty object so we need a check to not show the OS Details title
    * in the template if this is the case.
    */
-  hasOperatingSystemData: Ember.computed('details.result.operating_system', function () {
-    const os = this.get('details.result.operating_system');
+  hasOperatingSystemData: Ember.computed('resource.operating_system', function () {
+    const os = this.get('resource.operating_system');
     if (os && typeof os === 'object') {
       return Object.keys(os).length > 0;
     }
